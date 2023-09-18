c15233a81f8f83d8395d771388e07b3f2c7129cb
Harpreet Singh <107623909+Harpreet2004@users.noreply.github.com>
2023-09-18 20:02:10 +0530

    Update majority-element.cpp

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,6 +1,7 @@
 //TC = O(N LOG N) + O(N)
 //SC = O(N)
 
+//**BETTER
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -21,3 +22,42 @@ public:
         };
     }
 };
+
+
+//****OPTIMAL
+//TC = O(N) + O(N)
+//SC = O(1)
+
+    //***MOORE'S VOTING ALGORITHM 
+        //1.take cnt=0, element if cnt =0 intialise element with nums[i]
+        //2.if ele = n[i] then ++ the count
+        //3.else not similar the decrement
+        //4.loop through the array again to find if selected element
+        //appears more than n/2 then return that element
+        int cnt=0,element;
+
+        for(int i=0;i<nums.size();i++) {
+            if(cnt == 0) {
+                cnt =1;
+                element = nums[i];
+            }
+
+            else if(nums[i] == element) {
+                cnt++;
+            }
+            
+            else {
+                cnt--;
+            }
+        }
+
+        int cnt1=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i] == element) {
+                cnt1++;
+            }
+            if(cnt1 > (nums.size()/2)) {
+                break;
+            }
+        }
+return element;
