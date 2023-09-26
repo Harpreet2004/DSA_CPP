4709c5d5459be2855d9df8cbd0c461c5e572d8ff
Harpreet Singh <107623909+Harpreet2004@users.noreply.github.com>
2023-09-26 17:44:47 +0530

    Time: 8 ms (11.69%) | Memory: 12.6 MB (7.64%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        //BRUTE**** CONTAINS RECRUSION IN STRIVER VID
+
+        //BETTER**** FOR C++ STL USERS ONLY
+        next_permutation(nums.begin(),nums.end());
+        
+        //OPTIMAL****
+         
+
+    }
+};
