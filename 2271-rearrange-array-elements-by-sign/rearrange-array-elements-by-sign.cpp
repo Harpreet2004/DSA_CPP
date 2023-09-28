05174e4cc3e7a54e52784dfe65e5a31ffe1ef6e1
Harpreet Singh <107623909+Harpreet2004@users.noreply.github.com>
2023-09-28 19:38:21 +0530

    Time: 155 ms (73.70%) | Memory: 124.4 MB (42.65%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        //BRUTE****
+        //TC = O(N) + O(N)
+        //SC = O(N)
+
+        // vector<int> pos;
+        // vector<int> neg;
+
+        // for(int i=0;i<nums.size();i++) {
+        //     if(nums[i] < 0) {
+        //         neg.push_back(nums[i]);
+        //     }
+        //     else{
+        //         pos.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0;i<nums.size()/2;i++) {
+        //     nums[2*i] = pos[i];
+        //     nums[2*i+1] = neg[i];
+        // }
+
+        // return {nums};
+
+        //OPTIMAL****
         int n= nums.size();
         vector<int> ans(n,0);
         
